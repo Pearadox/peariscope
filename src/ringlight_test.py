cfcc709c739dd091d20fc60c7c85752f8d5383d8
Brad Prevost <brad.prevost@yahoo.com>
2020-01-19 23:09:18 -0800

    Move example scripts that are not part of main program.

--- src/ringlight_test.py
+++ src/ringlight_test.py
@@ -4,19 +4,13 @@ import board
 import neopixel
 import time
 
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-
 NUMPIXELS = 16
 pixels = neopixel.NeoPixel(board.D18, NUMPIXELS)
 
-pixels.fill(BLACK) # Turn off pixels
+pixels.fill((0, 0, 0)) # Turn off pixels
 
 while True:
     for i in range(NUMPIXELS-1, -1, -1):
-        pixels[i] = RED
+        pixels[i] = (255, 0, 0) # Red
         time.sleep(1/NUMPIXELS)
-        pixels[i] = GREEN
+        pixels[i] = (0, 255, 0) # Green
